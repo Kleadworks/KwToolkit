7522dada3f1b022adc2f4ed8dcf4643f8749fcf8
Kleadron <kleadron04@gmail.com>
2026-01-23 16:52:50 -0800

    my custom export profiles

---
+++ profile_editor.py
@@ -0,0 +1,22 @@
+# custom.py
+
+target="release_debug"
+bits="32"
+tools="yes"
+platform="windows"
+#optimize="speed"
+#debug_symbols="yes"
+#lto="none"
+
+#production = "yes"
+#minizip = "no"
+deprecated = "no"
+#disable_advanced_gui="yes"
+modules_enabled_by_default = "yes"
+
+#module_freetype_enabled = "yes"
+#module_gdscript_enabled = "yes"
+#module_stb_vorbis_enabled = "yes"
+#module_vorbis_enabled = "yes"
+#module_minimp3_enabled = "yes"
+#module_webp_enabled = "yes"
---
+++ profile_export.py
@@ -0,0 +1,22 @@
+# custom.py
+
+target="release"
+bits="32"
+tools="no"
+platform="windows"
+optimize="size"
+debug_symbols="no"
+#lto="full"
+
+production = "yes"
+minizip = "no"
+deprecated = "no"
+disable_advanced_gui="yes"
+modules_enabled_by_default = "no"
+
+#module_freetype_enabled = "yes"
+module_gdscript_enabled = "yes"
+module_stb_vorbis_enabled = "yes"
+module_vorbis_enabled = "yes"
+module_minimp3_enabled = "yes"
+module_webp_enabled = "yes"
